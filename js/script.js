17ddf1ae1458dbc901ec32b5ea3ed22b23c3eeb1
Flotib <flotib@hotmail.fr>
2021-08-18 23:46:46 +0200

    Beautify the code uwu

--- js/script.js
+++ js/script.js
@@ -5,16 +5,16 @@ var app = new Vue({
 		openBag: true,
 		itemGiverId: 0,
 		selectedItem: {
-				selection: false,
-				item: [],
-				slotId: null,
-				target: null,
+			selection: false,
+			item: [],
+			slotId: null,
+			target: null,
 		},
 		player: {
 			type: 'player', //could be a seller or something else
 			bag: {
 				level: 1,
-				bagSpace: 5,
+				bagSpace: 8,
 				slots: [],
 			},
 			equipment: {
@@ -40,7 +40,7 @@ var app = new Vue({
 			},
 		]
 	},
-	
+
 	watch: {
 		'player.bag.bagSpace': function (slots, oldslots) {
 			this.changeSlots(this.player.bag.slots, slots)
@@ -48,14 +48,36 @@ var app = new Vue({
 	},
 
 	computed: {
-		
+		unequipButtonStyles() {
+			let color = 'gray';
+
+			if (this.player.equipment.item.length > 0 && this.emptySpace(this.player.bag.slots) > 0) {
+				color = 'green';
+			}
+
+			return {
+				backgroundColor: color,
+			}
+		},
+
+		equipButtonStyles() {
+			let color = 'gray'
+
+			if (this.selectedItem.target.type == 'player' && this.selectedItem.item[0].equipable) {
+				color = 'green';
+			}
+
+			return {
+				backgroundColor: color,
+			}
+		},
 	},
 
 	methods: {
 
 		getItemName(id) {
 			let index = this.items.findIndex(item => item.id === id)
-			if (index >= 0 ) {
+			if (index >= 0) {
 				return this.items[index].name
 			}
 		},
@@ -77,7 +99,7 @@ var app = new Vue({
 
 		changeSlots(target, slots) {
 			if (target.length < slots) {
-				target.push({slotId: target.length+1, content: null})
+				target.push({ slotId: target.length + 1, content: null })
 			} else if (target.length > slots) {
 				target.pop()
 			}
@@ -89,16 +111,19 @@ var app = new Vue({
 
 		emptySpace(target) {
 			let emptySlots = 0
-			for(let i=0, l = target.length; i < l; i++){
+			for (let i = 0, l = target.length; i < l; i++) {
 				emptySlots += (target[i].content === null) ? 1 : 0;
 			}
 			return emptySlots
 		},
 
 		deleteItem(x) {
-			if (this.player.bag.slots[x-1].content != null) {
-				this.player.bag.slots.splice(x-1, 1, {slotId: x, content: null})
+			if (x == this.player.equipment.slotId) {
+				this.player.equipment.item.splice(0, 1)
+			} else if (this.player.bag.slots[x - 1].content != null) {
+				this.player.bag.slots.splice(x - 1, 1, { slotId: x, content: null })
 			}
+			this.unselectItem()
 		},
 
 		itemSelection(item, slotId, target) {
@@ -113,11 +138,11 @@ var app = new Vue({
 				let actualItem = this.player.equipment.item[0]
 				actualItem.equipable = true
 				this.player.equipment.item.splice(0, 1, item)
-				this.player.bag.slots.splice(slot-1, 1, {slotId: slot, content: actualItem})
+				this.player.bag.slots.splice(slot - 1, 1, { slotId: slot, content: actualItem })
 				this.player.equipment.item[0].equipable = false
 			} else {
 				this.player.equipment.item.splice(0, 1, item)
-				this.player.bag.slots.splice(slot-1, 1, {slotId: slot, content: null})
+				this.player.bag.slots.splice(slot - 1, 1, { slotId: slot, content: null })
 				this.player.equipment.item[0].equipable = false
 			}
 			this.unselectItem()
@@ -128,14 +153,14 @@ var app = new Vue({
 			let actualItem = this.player.equipment.item[0]
 			actualItem.equipable = true
 			this.player.equipment.item.splice(0, 1)
-			this.player.bag.slots.splice(emptySlot, 1, {slotId: emptySlot+1, content: actualItem})
+			this.player.bag.slots.splice(emptySlot, 1, { slotId: emptySlot + 1, content: actualItem })
 			this.unselectItem()
 			console.log(this.player.equipment.item)
 		},
 
 		unselectItem() {
 			this.selectedItem.selection = false,
-			this.selectedItem.item.shift()
+				this.selectedItem.item.shift()
 			this.selectedItem.slotId = null
 			this.selectedItem.target = null
 		},
@@ -147,6 +172,6 @@ var app = new Vue({
 
 		setInterval(() => {
 
-		}, 1000/this.fps)
+		}, 1000 / this.fps)
 	},
 })
